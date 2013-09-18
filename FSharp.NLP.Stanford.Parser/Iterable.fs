04853d4a39e53d224b53d7849a01c9fc33775651
jliettehk <cbeaucousin@gmail.com>
2013-09-18 21:31:06 +0200

    Update Iterable.fs
    Hello Sergey,
    Thanks a lot for your NLP initiatives. I made 2 small changes to toSeq fun because when I played with your demo, I raised a few Exceptions coming from Iterable.toSeq. Cheers.

--- FSharp.NLP.Stanford.Parser/Iterable.fs
+++ FSharp.NLP.Stanford.Parser/Iterable.fs
@@ -4,14 +4,18 @@ open java.lang
 open java.util
 
 module Iterable = 
+    // 2 changes proposed in toSeq function :
+    // 1- yield moved after if x.hasNext()
+    // 2- force a copy (as proposed by Tomasp on SO : http://stackoverflow.com/questions/6605253/wrapping-a-mutable-collection-with-a-sequence
     let toSeq (iter:Iterable) =
         let rec loop (x:Iterator) = 
            seq { 
-                yield x.next()
                 if x.hasNext() then 
+                    yield x.next()  
                     yield! (loop x)
                 }
-        iter.iterator() |> loop
+        iter.iterator() |> loop |> Array.ofSeq |> Seq.readonly
+
 
     let castToSeq<'T> iter =
         toSeq iter
